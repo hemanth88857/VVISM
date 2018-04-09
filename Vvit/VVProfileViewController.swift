6715ea4f54f181f4f3aaad04e6163721e499e30f
hemanth kumar <hemanthkumar@Apples-MacBook-Pro.local>
2018-04-09 10:18:52 +0530

    Intial commit

---
+++ Vvit/VVProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VVProfileViewController.swift
+//  Vvit
+//
+//  Created by hemanth kumar on 4/9/18.
+//  Copyright © 2018 hemanth kumar. All rights reserved.
+//
+
+import UIKit
+
+class VVProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
