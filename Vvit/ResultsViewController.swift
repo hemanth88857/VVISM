612d26b42ff1d152a91a6137a5b0e133af42fd51
hemanth kumar <hemanthkumar@apples-macbook-pro.local>
2018-04-17 10:00:48 +0530

    results page

---
+++ Vvit/ResultsViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ResultsViewController.swift
+//  Vvit
+//
+//  Created by hemanth kumar on 4/14/18.
+//  Copyright © 2018 hemanth kumar. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Results"
+    }
+    // MARK: Results Actions
+    @IBAction func MBACDE(_ sender: Any) {
+        guard let url = URL(string: "http://www.osmania.ac.in/res07/20180522.jsp") else {
+            return //be safe
+        }
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+
+    }
+    @IBAction func MCA_RV(_ sender: Any) {
+        guard let url = URL(string: "http://www.osmania.ac.in/res07/20180515.jsp") else {
+            return //be safe
+        }
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+
+    }
+    
+    @IBAction func BCACBCS(_ sender: Any) {
+        guard let url = URL(string: "http://www.osmania.ac.in/res07/20180510.jsp") else {
+            return //be safe
+        }
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+
+    }
+    
+    @IBAction func MCA_CBCSaug2017(_ sender: Any) {
+        guard let url = URL(string: "http://www.osmania.ac.in/res07/20180519.jsp") else {
+            return //be safe
+        }
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
